bd56da670d7d5088296f3342cfd0266e7c4433b5
FARDIM KAISER <fardim_drmc_9094@yahoo.com>
2019-11-14 02:58:57 +0600

    End of section 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        values: []
+    };
+    componentDidMount() {
+        axios.get("https://localhost:5001/WeatherForecast").then(response => {
+            this.setState({
+                values: response.data
+            });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Header as="h2" icon="users" content="Reactivities" />
+                <List>
+                    {this.state.values.map((value: any) => (
+                        <List.Item key={value.temperatureC + value.date}>
+                            {value.summary}
+                        </List.Item>
+                    ))}
+                </List>
+            </div>
+        );
+    }
 }
 
 export default App;
